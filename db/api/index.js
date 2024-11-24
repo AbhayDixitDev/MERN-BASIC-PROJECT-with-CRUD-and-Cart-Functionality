a74436875d3de6d0b1a0b79c5b92d578c93ad86d
Abhay Dixit <dixitshivam534@gmail.com>
2024-11-25 01:31:51 +0530

    changes

--- db/api/index.js
+++ db/api/index.js
@@ -6,18 +6,23 @@ const productRoute = require('./routes/productRoute');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.DBLINK, { useNewUrlParser: true, useUnifiedTopology: true });
 
+mongoose.connect(process.env.DBLINK, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err.message);
+    process.exit(1);
+  });
 
 app.use('/products', productRoute);
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Welcome to the API');
 });
 
 app.listen(port, () => {
